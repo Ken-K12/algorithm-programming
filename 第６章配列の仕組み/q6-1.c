324c42f7bc0a919a391ef282bb7406f1ea172d74
Ken-K12 <k-12major28@i.softbank.jp>
2023-01-04 13:19:30 +0900

    第６章練習問題

---
+++ 第６章配列の仕組み/q6-1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    float a[10];
+    int i;
+    float sum, avg;
+
+    a[0] = 30.0;
+    a[1] = 20.0;
+    a[2] = 10.0;
+    a[3] = 25.0;
+    a[4] = 15.0;
+    sum = 0.0;
+
+    for (i=0; i<5; i++)
+     sum += a[i];
+    
+    avg = sum / 5.00;
+    printf ("%f\n", avg);
+
+    return 0;
+
+}
